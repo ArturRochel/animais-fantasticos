13a6b87f22c97fc94850990428771e8fe6425884
arturrochel <arturrochel@gmail.com>
2024-01-24 17:37:23 -0300

    curso JavaScript

--- script.js
+++ script.js
@@ -82,3 +82,76 @@ function initAnimationScroll() {
   }
 }
 initAnimationScroll();
+
+// Utilizando o foreach na array abaixo,
+// some os valores de Taxa e os valores de Recebimento
+
+const transacoes = [
+  {
+    descricao: 'Taxa do Pão',
+    valor: 'R$ 39',
+  },
+  {
+    descricao: 'Taxa do Mercado',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 99',
+  },
+  {
+    descricao: 'Taxa do Banco',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 49',
+  },
+];
+
+let valueTax = 0
+let valueReci = 0
+
+transacoes.forEach((item) => {
+  const clearNumber = +item.valor.replace('R$ ', '')
+  if(item.descricao.slice(0, 4) === 'Taxa'){
+    valueTax += clearNumber
+  } else if(item.descricao.slice(0, 4) === 'Rece'){
+    valueReci += clearNumber
+  }
+})
+
+console.log(`Esse é o valor total de taxa: R$ ${valueTax}`)
+console.log(`Esse é o valor total de recebimentos: R$ ${valueReci}`)
+// Retorne uma array com a lista abaixo
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+const transportesArray = transportes.split(';')
+console.log(transportesArray)
+// Substitua todos os span's por a's
+const html = `<ul>
+                <li><span>Sobre</span></li>
+                <li><span>Produtos</span></li>
+                <li><span>Contato</span></li>
+              </ul>`;
+const htmlAs = html.split('span')
+const htmlNovo = htmlAs.join('section')
+// Retorne o último caracter da frase
+const frase = 'Melhor do ano!';
+const oUltimo = frase[frase.length - 1]
+console.log(oUltimo)
+// Retorne o total de taxas
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+
+let totalTaxas = 0
+let outrasTaxas = 0
+transacoes2.forEach((item) => {
+  const fraseLimpa = item.toLowerCase()
+  if(fraseLimpa.includes('taxa')){
+    totalTaxas += 1
+  } else {
+    outrasTaxas += 1
+  }
+})
+
+console.log(totalTaxas)
+console.log(outrasTaxas)
